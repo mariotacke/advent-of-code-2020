f03eb47271eb02c21f4067863aee8b4d4c93409f
Mario Tacke <tacke.mario@gmail.com>
2020-12-04 22:31:25 -0800

    perf: improve Day 5: Binary Boarding

--- day-05-binary-boarding/boarding.js
+++ day-05-binary-boarding/boarding.js
@@ -1,9 +1,5 @@
 module.exports = (input) => {
-  return input.split('\n').map((line) => {
-    const bits = line.split('').map((bit) => /F|L/.test(bit) ? 0 : 1).join('');
-    const row = parseInt(bits.slice(0, 7), 2);
-    const column = parseInt(bits.slice(7), 2);
-
-    return row * 8 + column;
-  }).sort((a, b) => b - a)[0];
+  return Math.max(...input.split('\n').map((line) => {
+    return parseInt(line.replace(/F|L/g, 0).replace(/B|R/g, 1), 2);
+  }));
 };
--- day-05-binary-boarding/boarding2.js
+++ day-05-binary-boarding/boarding2.js
@@ -1,10 +1,6 @@
 module.exports = (input) => {
   const boardingPasses = new Set([...input.split('\n').map((line) => {
-    const bits = line.split('').map((bit) => /F|L/.test(bit) ? 0 : 1).join('');
-    const row = parseInt(bits.slice(0, 7), 2);
-    const column = parseInt(bits.slice(7), 2);
-
-    return row * 8 + column;
+    return parseInt(line.replace(/F|L/g, 0).replace(/B|R/g, 1), 2);
   })]);
 
   for (const seatId of boardingPasses.values()) {

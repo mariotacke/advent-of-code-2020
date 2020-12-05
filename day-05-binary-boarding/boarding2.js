aa142820f84262c38fa63a7b6a8e9bb134e18d9c
Mario Tacke <tacke.mario@gmail.com>
2020-12-05 11:12:04 -0800

    chore: remove unnecessary array spread for Day 5, Part 2

--- day-05-binary-boarding/boarding2.js
+++ day-05-binary-boarding/boarding2.js
@@ -1,7 +1,7 @@
 module.exports = (input) => {
-  const boardingPasses = new Set([...input.split('\n').map((line) => {
+  const boardingPasses = new Set(input.split('\n').map((line) => {
     return parseInt(line.replace(/F|L/g, 0).replace(/B|R/g, 1), 2);
-  })]);
+  }));
 
   for (const seatId of boardingPasses.values()) {
     if (!boardingPasses.has(seatId - 1) && boardingPasses.has(seatId - 2)) {
